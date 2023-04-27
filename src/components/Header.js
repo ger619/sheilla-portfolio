76f154c6cdc4e88d80ea5f89e475a02abedda8df
ger619 <draker74@gmail.com>
2023-04-27 18:01:34 +0300

    not working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Logo from '../assets/logo.png';
 
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return <header className="bg-pink-300" py-8>
+    <div className="container mx-auto">
+      <a href="#">
+         <img src={Logo} />
+      </a>
+
+    </div>
+  </header>;
 };
 
 export default Header;
