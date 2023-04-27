ccf3626931996d0bfb1c67628539777f8dec065c
ger619 <draker74@gmail.com>
2023-04-27 18:27:37 +0300

    Add all

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Logo from '../assets/logo.png';
 
 const Header = () => {
-  return <header className="bg-pink-300" py-8>
+  return <header className="bg-white py-8">
     <div className="container mx-auto">
-      <a href="#">
-         <img src={Logo} />
-      </a>
+      <div className="flex justify-between items-center">
+        <a href="#">
+          <img className="w-20" src={Logo} />
+        </a>
+        <button className="btn btn-sm">Work With Me</button>
 
+      </div>
     </div>
   </header>;
 };
